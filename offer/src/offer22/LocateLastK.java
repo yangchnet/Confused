21b1b2425eba70ac769b374b739879a64f7fbe76
yangchnet <1048887414@qq.com>
2020-03-01 22:21:22 +0800

    3-1

---
+++ offer/src/offer22/LocateLastK.java
@@ -0,0 +1,17 @@
+package offer22;
+
+public class LocateLastK {
+
+     public static class ListNode {
+         int val;
+         ListNode next;
+
+         public ListNode(int x) {
+             val = x;
+         }
+     }
+
+    public ListNode getKthFromEnd(ListNode head, int k) {
+
+    }
+}
