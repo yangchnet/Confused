9ddbbac3924369583858aa838c20b69aa5a4af02
yangchnet <1048887414@qq.com>
2021-03-28 22:07:18 +0800

    3-28

--- Golang/test/1.go
+++ Golang/test/1.go
@@ -2,14 +2,47 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
-	"strings"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 )
 
 func main() {
-	reader := io.TeeReader(strings.NewReader("Go语言中文网"), os.Stdout)
-	n, err := reader.Read(make([]byte, 20))
-	fmt.Println(n, err)
+	sigRecv1 := make(chan os.Signal, 1)
+	sigs1 := []os.Signal{syscall.SIGINT, syscall.SIGQUIT}
+	fmt.Printf("Set notification for %s... [sigRecv1]\n", sigs1)
+	signal.Notify(sigRecv1, sigs1...)
+
+	sigRecv2 := make(chan os.Signal, 1)
+	sigs2 := []os.Signal{syscall.SIGQUIT}
+	fmt.Printf("Set notification for %s... [sigRecv2]\n", sigs2)
+	signal.Notify(sigRecv2, sigs2...)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func() {
+		for sig := range sigRecv1 {
+			fmt.Printf("Received a signal from sigRecv1:%s\n", sig)
+		}
+		fmt.Printf("End. [sigRecv1]\n")
+		wg.Done()
+	}()
+
+	go func() {
+		for sig := range sigRecv2 {
+			fmt.Printf("Received a signal from sigRecv2:%s\n", sig)
+		}
+		fmt.Printf("End. [sigRecv2]\n")
+		wg.Done()
+	}()
+
+	fmt.Println("Wait for 2 seconds...")
+	time.Sleep(2 * time.Second)
+	fmt.Printf("Stop notification...")
+	signal.Stop(sigRecv1)
+	//signal.Stop(sigRecv2)
+	close(sigRecv1)
 
 }
